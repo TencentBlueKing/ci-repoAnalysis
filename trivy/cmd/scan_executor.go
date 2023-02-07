73d08c7a23f8f8515061edb22aa5bfb1133aba89
kunlongli <kunlongli@tencent.com>
2023-02-07 22:13:26 +0800

    feat: 支持配置trivy-java-db下载路径，修改下载的漏洞库文件类型为tar.gz #10

--- trivy/cmd/scan_executor.go
+++ trivy/cmd/scan_executor.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"archive/tar"
 	"bytes"
+	"compress/gzip"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -13,13 +15,12 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strconv"
 )
 
 type TrivyExecutor struct{}
 
 func (e TrivyExecutor) Execute(config *object.ToolConfig, file *os.File) (*object.ToolOutput, error) {
-	if err := downloadDB(config); err != nil {
+	if err := downloadAllDB(config); err != nil {
 		return nil, err
 	}
 	maxTime, err := config.GetIntArg("maxTime")
@@ -34,26 +35,99 @@ func (e TrivyExecutor) Execute(config *object.ToolConfig, file *os.File) (*objec
 	return transformOutputJson()
 }
 
-func downloadDB(config *object.ToolConfig) error {
+func downloadAllDB(config *object.ToolConfig) error {
 	url := config.GetStringArg("dbDownloadUrl")
+	if err := downloadDB(url, constant.DbDir); err != nil {
+		return err
+	}
+	javaDbUrl := config.GetStringArg("javaDbDownloadUrl")
+	if err := downloadDB(javaDbUrl, constant.JavaDbDir); err != nil {
+		return err
+	}
+	return nil
+}
+
+func downloadDB(url string, dbDir string) error {
 	response, err := http.DefaultClient.Get(url)
 	if err != nil {
 		return err
 	}
 	defer response.Body.Close()
 	if response.StatusCode != 200 {
-		return errors.New("download failed")
+		return errors.New(fmt.Sprintf("download db to %s failed, status %s", dbDir, response.Status))
 	}
-	file, err := os.Create(filepath.Join(constant.DbCacheDir, constant.DbFilename))
+	p := filepath.Join(constant.DbCacheDir, dbDir, "temp.tar")
+	if err := os.MkdirAll(filepath.Dir(p), 0666); err != nil {
+		return err
+	}
+	file, err := os.Create(p)
 	if err != nil {
 		return err
 	}
-	defer file.Close()
 	written, err := io.Copy(file, response.Body)
 	if err != nil {
 		return err
 	}
-	util.Info("download db, size is " + strconv.FormatInt(written, 10))
+	if err := file.Close(); err != nil {
+		return err
+	}
+	if err := extract(p); err != nil {
+		return err
+	}
+	if err := os.Remove(p); err != nil {
+		return err
+	}
+	util.Info("download db to %s success, size is %d", p, written)
+	return nil
+}
+
+func extract(tarPath string) error {
+	gzipStream, err := os.Open(tarPath)
+	if err != nil {
+		return err
+	}
+	defer gzipStream.Close()
+	uncompressedStream, err := gzip.NewReader(gzipStream)
+	if err != nil {
+		return err
+	}
+	defer uncompressedStream.Close()
+	tarReader := tar.NewReader(uncompressedStream)
+
+	dir := filepath.Dir(tarPath)
+	for true {
+		header, err := tarReader.Next()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return err
+		}
+
+		p := filepath.Join(dir, header.Name)
+		switch header.Typeflag {
+		case tar.TypeDir:
+			if err := os.Mkdir(p, 0444); err != nil {
+				return err
+			}
+		case tar.TypeReg:
+			outFile, err := os.Create(p)
+			if err != nil {
+				return err
+			}
+			if _, err := io.Copy(outFile, tarReader); err != nil {
+				return err
+			}
+			if err := outFile.Close(); err != nil {
+				return err
+			}
+		default:
+			return errors.New(fmt.Sprintf("unknown tar type %d in %s", header.Typeflag, header.Name))
+		}
+	}
+
 	return nil
 }
 
@@ -69,6 +143,7 @@ func execTrivy(fileName string, maxTime int64, scanSensitive bool) error {
 		constant.FlagOutput, constant.OutputPath,
 		constant.FlagTimeout, fmt.Sprintf("%dms", maxTime),
 		constant.FlagSkipDbUpdate,
+		constant.FlagSkipJavaDbUpdate,
 		constant.FlagOfflineScan,
 	}
 
