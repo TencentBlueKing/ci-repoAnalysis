28b879f89f2b4e285674353d2824b4a531fe2d45
kunlongli <kunlongli@tencent.com>
2023-03-23 17:48:23 +0800

    feat: 支持dependency-check离线扫描 #23

--- dependency-check/pkg/constant.go
+++ dependency-check/pkg/constant.go
@@ -2,3 +2,12 @@ package pkg
 
 // CMDDependencyCheck 命令
 const CMDDependencyCheck = "/usr/share/dependency-check/bin/dependency-check.sh"
+
+// DirDependencyCheckData 漏洞库存放目录
+const DirDependencyCheckData = "/usr/share/dependency-check/data"
+
+// ConfigOffline 是否使用离线模式
+const ConfigOffline = "offline"
+
+// ConfigDbUrl 漏洞库下载地址
+const ConfigDbUrl = "dbUrl"
