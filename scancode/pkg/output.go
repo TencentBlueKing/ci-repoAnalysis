3acd1eba486c61ee5f383725b134d6dba0a774bd
kunlongli <kunlongli@tencent.com>
2023-06-16 22:06:18 +0800

    feat: 增加scancode扫描工具 #21

--- scancode/pkg/output.go
+++ scancode/pkg/output.go
@@ -12,17 +12,12 @@ type Report struct {
 
 // Item Scancode扫描结果项
 type Item struct {
-	Path                    string    `json:"path"`
-	Type                    string    `json:"type"`
-	Licenses                []SubItem `json:"licenses"`
-	LicenseExpressions      []string  `json:"license_expressions"`
-	PercentageOfLicenseText float64   `json:"percentage_of_license_text"`
-	ScanErrors              []string  `json:"scan_errors"`
-}
-
-// SubItem 扫描到的License
-type SubItem struct {
-	SpdxLicenseKey string `json:"spdx_license_key"`
+	Path                    string   `json:"path"`
+	Type                    string   `json:"type"`
+	SpdxLicenseKey          string   `json:"detected_license_expression_spdx"`
+	LicenseExpressions      []string `json:"license_expressions"`
+	PercentageOfLicenseText float64  `json:"percentage_of_license_text"`
+	ScanErrors              []string `json:"scan_errors"`
 }
 
 // Convert 转换工具输出的报告为制品库需要的报告
@@ -33,9 +28,10 @@ func Convert(report *Report) *object.Result {
 
 	for i := range report.Files {
 		file := report.Files[i]
-		for j := range file.Licenses {
-			license := file.Licenses[j].SpdxLicenseKey
-			path := file.Path[strings.Index(file.Path, "/")+1:]
+		path := file.Path[strings.Index(file.Path, "/")+1:]
+		licenses := parseLicense(file.SpdxLicenseKey)
+		for j := range licenses {
+			license := licenses[j]
 			licenseResult := object.LicenseResult{
 				LicenseName: license,
 				Path:        path,
@@ -49,4 +45,22 @@ func Convert(report *Report) *object.Result {
 	}
 
 	return result
+}
+
+func parseLicense(detectedLicense string) []string {
+	splitLicenses := strings.Split(detectedLicense, " ")
+	licenses := make([]string, 0)
+	for i := range splitLicenses {
+		l := splitLicenses[i]
+		if l == "AND" || l == "OR" {
+			continue
+		} else if l[0] == '(' {
+			licenses = append(licenses, l[1:])
+		} else if l[len(l)-1] == ')' {
+			licenses = append(licenses, l[0:len(l)-1])
+		} else {
+			licenses = append(licenses, l)
+		}
+	}
+	return licenses
 }
