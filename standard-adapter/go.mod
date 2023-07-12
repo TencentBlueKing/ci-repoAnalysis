2da111c8f0eb8e80b73400fe63c2dcbd6beeaf9a
kunlongli <16629885+cnlkl@users.noreply.github.com>
2023-07-12 16:53:05 +0800

    feat: 支持将离线的标准扫描器转为在线扫描器 #48

---
+++ standard-adapter/go.mod
@@ -0,0 +1,5 @@
+module github.com/TencentBlueKing/ci-repoAnalysis/standard-adapter
+
+go 1.20
+
+require github.com/TencentBlueKing/ci-repoAnalysis/analysis-tool-sdk-golang v0.0.18
---
+++ standard-adapter/pkg/adapter.go
@@ -0,0 +1,80 @@
+package pkg
+
+import (
+	"encoding/json"
+	"github.com/TencentBlueKing/ci-repoAnalysis/analysis-tool-sdk-golang/api"
+	"github.com/TencentBlueKing/ci-repoAnalysis/analysis-tool-sdk-golang/object"
+	"github.com/TencentBlueKing/ci-repoAnalysis/analysis-tool-sdk-golang/util"
+	"os"
+)
+
+// StandardAdapterExecutor 标准扫描器适配器
+type StandardAdapterExecutor struct {
+	cmd     string
+	workDir string
+}
+
+// Execute 执行扫描
+func (e StandardAdapterExecutor) Execute(_ *object.ToolConfig, file *os.File) (*object.ToolOutput, error) {
+	// 将toolInput写入/bkrepo/workspace/input.json
+	toolInput, err := api.GetClient(object.GetArgs()).Start()
+	if err != nil {
+		return nil, err
+	}
+	newToolInput := &object.ToolInput{
+		TaskId:     toolInput.TaskId,
+		ToolConfig: toolInput.ToolConfig,
+		FilePath:   file.Name(),
+		Sha256:     "",
+		FileUrls:   nil,
+	}
+	toolInputFile := util.WorkDir + "/input.json"
+	if err := e.writeToolInput(newToolInput, toolInputFile); err != nil {
+		return nil, err
+	}
+
+	// 执行扫描
+	toolOutputFile := util.WorkDir + "/output.json"
+	args := []string{
+		"--input", toolInputFile,
+		"--output", toolOutputFile,
+	}
+	err = util.ExecAndLog(e.cmd, args, e.workDir)
+	if err != nil {
+		return nil, err
+	}
+
+	// 从/bkrepo/workspace/output.json读取扫描结果
+	return e.readToolOutput(toolOutputFile)
+}
+
+func (e StandardAdapterExecutor) writeToolInput(input *object.ToolInput, f string) error {
+	file, err := os.Create(f)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(input)
+	if err != nil {
+		return err
+	}
+	util.Info("write input.json success")
+	return nil
+}
+
+func (e StandardAdapterExecutor) readToolOutput(f string) (*object.ToolOutput, error) {
+	file, err := os.Open(f)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	output := new(object.ToolOutput)
+	err = json.NewDecoder(file).Decode(output)
+	if err != nil {
+		return nil, err
+	}
+	return output, nil
+}
